3e1e8fd7e3f01fb961e87628b7b94214b57fc3b7
K Mak <kumarajiva08@gmail.com>
2018-06-30 22:02:09 +1000

    Bootstrap, read data from json file

--- src/App.js
+++ src/App.js
@@ -5,30 +5,7 @@ import Watchlist from './Watchlist';
 
 
 class App extends Component {
-  state = {
-    watchlist: [
-      {
-        "stock": "Asaleo Care",
-        "code": "AHY",
-      },
-      {
-        "stock": "TPG Telecom",
-        "code": "TPM",
-      },
-      {
-        "stock": "Pact Group",
-        "code": "PGH",
-      },
-      {
-        "stock": "Wesfarmers",
-        "code": "WES",
-      },
-      {
-        "stock": "National Australia Bank",
-        "code": "NAB",
-      },
-    ]
-  }
+  state = require('./data.json');
   render() {
     return (
       <Watchlist watchlist={this.state.watchlist} />
--- src/Watchlist.js
+++ src/Watchlist.js
@@ -5,20 +5,24 @@ class Watchlist extends Component {
     console.log(this.props);
     return (
       <div>
-        <table>
-          <tbody>
-            <tr>
-              <th>Stock</th>
-              <th>ASX Code</th>
-            </tr>
-            {this.props.watchlist.map((rec) => (
-              <tr key={rec.code}>
-                <td>{rec.stock}</td>
-                <td>{rec.code}</td>
+        <div className="container mt-3">
+          <table className="table">
+            <thead className="thead-light">
+              <tr>
+                <th>Stock</th>
+                <th>ASX Code</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {this.props.watchlist.map((rec) => (
+                <tr key={rec.code}>
+                  <td>{rec.stock}</td>
+                  <td>{rec.code}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     )
   }
